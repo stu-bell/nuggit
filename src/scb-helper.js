598451a107f5d65ee39b0ac39b5c94acd6b2d76a
Stuart Bell <stuart.bell@bluefinsolutions.com>
2017-07-27 18:38:19 +0100

    get object name based on node type

--- src/scb-helper.js
+++ src/scb-helper.js
@@ -1,7 +1,13 @@
 var R = require('ramda');
 
 var S = {
-	nuggAdd: (oNugg, aNodes) => R.map(oNode => oNugg.childNodes[1].appendChild(oNode), aNodes)	// assume the nugget node is the first childnode
+	nuggAdd: (oNugg, aNodes) => R.map(oNode => oNugg.childNodes[1].appendChild(oNode), aNodes),	// assume the nugget node is the first childnode
+	getNameAttribute: R.cond([
+		[R.propEq('nodeName', 'CLAS'), R.always('CLSNAME')],
+		[R.propEq('nodeName', 'PROG'), R.always('NAME')],
+		[R.T, R.always('NAME')] // else try NAME
+	]) // looks up the type of a node, returns the key of the attribute containing the ABAP object name
 };
+S.getObjectName = oNode => oNode.getAttribute(S.getNameAttribute(oNode));
 
 module.exports = S;
