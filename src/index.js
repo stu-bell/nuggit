d2141e30d898b8a621c609b236ee4d02d5cdd20b
Stuart Bell <stuart.bell@bluefinsolutions.com>
2017-07-27 14:34:48 +0100

    read and console a file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 var xpath = require('xpath'),
 	xdom = require('xmldom'),
+	fs = require('fs'),
 	sSourcePath = process.argv[2];
 
 console.log(sSourcePath);
 
-// fs.readFile
+fs.readFile(sSourcePath, 'utf8', (err, data) => {
+
+	console.log(data);
+});
