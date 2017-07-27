f4e790cef3304f1bc156fc6b50903686caa694c1
Stuart Bell <stuart.bell@bluefinsolutions.com>
2017-07-27 16:02:45 +0100

    serializeXML to string

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 #!/usr/bin/env node
-var xpath = require('xpath'),
-	xdom = require('xmldom'),
-	fs = require('fs'),
-	sSourcePath = process.argv[2];
+const xpath = require('xpath'),
+xmldom = require('xmldom').DOMParser,
+xmlserialize = require('xmldom').XMLSerializer,
+fs = require('fs'),
+R = require('ramda'),
+sSourcePath = process.argv[2],
 
-console.log(sSourcePath);
+t = {
+	msgProcessingFile: "Processing file: "
+};
 
+// log
+console.log(t.msgProcessingFile, sSourcePath);
+
+// process the main source file
 fs.readFile(sSourcePath, 'utf8', (err, data) => {
+var R = require('ramda'); // for debugging only -
 
-	console.log(data);
+	// error handlign
+	if (err) {
+		throw err;
+	}
+
+	// parse
+	var oDoc = new xmldom().parseFromString(data, 'application/xml'),
+
+	// select nodes
+	aNodes = xpath.select("//CLAS", oDoc);
+
+	// process each selected node
+	R.map(oNode => {
+		// serialize node to XML string
+		var sXML = new xmlserialize().serializeToString(oNode);
+		// write to file
+		console.log(sXML);
+	}, aNodes);
+
 });
